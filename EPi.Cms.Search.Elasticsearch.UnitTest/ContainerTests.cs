85ca8c1ed50aa8ea0d25222e4389aaa7cf97cbfe
Jeroen Slor <jeroen.slor@gmail.com>
2016-02-17 07:30:36 +0100

    Going home commit

---
+++ EPi.Cms.Search.Elasticsearch.UnitTest/ContainerTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Linq;
+using EPi.Cms.Search.Elasticsearch.Indexing;
+using EPiServer.Core;
+using StructureMap;
+using Xunit;
+
+namespace EPi.Cms.Search.Elasticsearch.UnitTest
+{
+    public class ContainerTests
+    {
+        [Fact]
+        public void GetAllPageDataIndexers_Should_Return_All_Indexers()
+        {
+            var container = new Container(_ =>
+            {
+                _.For<IPageDataIndexer<TestPage>>().Add<TestPageIndexer>();
+            });
+
+            var testPage = new TestPage();
+            var foo = new TestPageIndexer();
+            var bar = foo as IPageDataIndexer<PageData>; // this is not allowed since in parameters are not allowed to downcast
+            Assert.NotNull(bar);
+
+            var instances = container.GetAllInstances<IPageDataIndexer<PageData>>();
+
+            Assert.True(instances.Count() == 1);
+        }
+
+        public class TestPageIndexer : IPageDataIndexer<TestPage>
+        {
+            public IPageDataIndexModel CreateIndexModel(TestPage pageData, CultureInfo cultureInfo)
+            {
+                return null;
+            }
+        }
+
+        public class TestPage : PageData
+        {
+        }
+    }
+}
---
+++ src/EPi.Cms.Search.Elasticsearch/Indexing/IPageDataIndexer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using EPiServer.Core;
+
+namespace EPi.Cms.Search.Elasticsearch.Indexing
+{
+    public interface IPageDataIndexer<in T>
+    {
+        IPageDataIndexModel CreateIndexModel(T pageData, CultureInfo cultureInfo);
+    }
+
+    public interface IPageDataIndexer
+    {
+        IPageDataIndexModel CreateIndexModel(PageData pageData, CultureInfo cultureInfo);
+    }
+
+    public interface IPageDataIndexModel
+    {
+        Guid Id { get; set; }
+        ContentReference ContentReference { get; set; }
+    }
+}
